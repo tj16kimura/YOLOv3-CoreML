2d92e324ea5cbcefddf06419f1ccdec53c16c604
danma <danma@yunify.com>
2018-05-05 11:23:23 +0800

    first commit

--- Convert/coreml.py
+++ Convert/coreml.py
@@ -1,22 +1,14 @@
-# Conversion script for tiny-yolo-voc to Core ML.
-# Needs Python 2.7 and Keras 1.2.2
-
 import coremltools
 
-coreml_model = coremltools.converters.keras.convert(
-    'yad2k/model_data/tiny-yolo-voc.h5',
-    input_names='image',
-    image_input_names='image',
-    output_names='grid',
-    image_scale=1/255.)
+coreml_model = coremltools.converters.keras.convert('./model_data/yolo.h5', input_names='input1', image_input_names='input1', output_names=['output1', 'output2', 'output3'], image_scale=1/255.)
+
+coreml_model.input_description['input1'] = 'Input image'
+coreml_model.output_description['output1'] = 'The 13x13 grid (Scale1)'
+coreml_model.output_description['output2'] = 'The 26x26 grid (Scale2)'
+coreml_model.output_description['output3'] = 'The 52x52 grid (Scale3)'
 
 coreml_model.author = 'Original paper: Joseph Redmon, Ali Farhadi'
 coreml_model.license = 'Public Domain'
-coreml_model.short_description = "The Tiny YOLO network from the paper 'YOLO9000: Better, Faster, Stronger' (2016), arXiv:1612.08242"
+coreml_model.short_description = "The YOLOv3 network from the paper 'YOLOv3: An Incremental Improvement'"
 
-coreml_model.input_description['image'] = 'Input image'
-coreml_model.output_description['grid'] = 'The 13x13 grid with the bounding box data'
-
-print(coreml_model)
-
-coreml_model.save('../TinyYOLO-CoreML/TinyYOLO-CoreML/TinyYOLO.mlmodel')
+coreml_model.save('Yolov3.mlmodel')
